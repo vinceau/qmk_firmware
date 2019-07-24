06ecb90e1d1adfa42a00ca0a9b701015ea00bbd9
Vince Au <vinceau09@gmail.com>
2019-07-24 22:45:26 +1000

    Trigger vim layer using right space bar

--- keyboards/dz60/keymaps/vinceau/keymap.c
+++ keyboards/dz60/keymaps/vinceau/keymap.c
@@ -17,7 +17,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC, KC_BSLS,
 	KC_ESCAPE, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_ENT,
 	KC_LSFT, XXXX, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT, KC_UP, MO(L3),
-	KC_LCTL, KC_LALT, KC_LGUI, KC_SPC, LT(L1, KC_SPC), KC_SPC, KC_RGUI, KC_RALT, KC_LEFT, KC_DOWN, KC_RIGHT),
+	KC_LCTL, KC_LALT, KC_LGUI, KC_SPC, KC_SPC, LT(L2, KC_SPC), KC_RGUI, KC_RALT, KC_LEFT, KC_DOWN, KC_RIGHT),
 
 // Utility layer - RGB and multimedia control
 [L1] = LAYOUT_all(
@@ -27,6 +27,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	____, ____, RGB_SAI, RGB_SAD, ____, ____, ____, ____, ____, KC_END, KC_PGDN, ____, KC_MPLY, KC_VOLU, KC_MUTE,
 	____, RGB_VAI, RGB_VAD, ____, ____, ____, ____, ____, KC_MPRV, KC_VOLD, KC_MNXT),
 
+// LDUR Vim layer
 [L2] = LAYOUT_all(
 	____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____,
 	____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____,
