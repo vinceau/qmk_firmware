c913b53771e0dd4f2f9b809d8ad0fcc52b9f0d40
Vince Au <vinceau09@gmail.com>
2021-06-18 09:13:50 +1000

    feat: add right control to windows layer

--- keyboards/dz60/keymaps/vinceau/keymap.c
+++ keyboards/dz60/keymaps/vinceau/keymap.c
@@ -13,21 +13,21 @@
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
-// Base layer - ANSI QWERTY
+// Base (Windows) layer - ANSI QWERTY
 [BL] = LAYOUT_all(
 	KC_GRV, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_EQL, XXXX, KC_BSPC,         // 15
 	KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC, KC_BSLS,              // 14
 	LT(NL, KC_ESCAPE), KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_ENT,          // 13
 	KC_LSFT, XXXX, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT, KC_UP, KC_DEL,  // 15
-	KC_LCTL, KC_LGUI, KC_LALT, KC_SPC, MO(SL), LT(VL, KC_SPC), KC_RGUI, KC_RALT, KC_LEFT, KC_DOWN, KC_RIGHT),   // 11
+	KC_LCTL, KC_LGUI, KC_LALT, KC_SPC, MO(SL), LT(VL, KC_SPC), KC_RALT, KC_RCTL, KC_LEFT, KC_DOWN, KC_RIGHT),   // 11
 
-// Swap left GUI and left ALT for Mac
+// Mac layer - Swap left GUI and left ALT for Mac
 [ML] = LAYOUT_all(
 	____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, XXXX, ____,
 	____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____,
 	____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____,
 	____, XXXX, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____,
-	____, KC_LALT, KC_LGUI, ____, ____, ____, ____, ____, ____, ____, ____),
+	____, KC_LALT, KC_LGUI, ____, ____, ____, KC_RGUI, KC_RALT, ____, ____, ____),
 
 
 // Number pad layer
