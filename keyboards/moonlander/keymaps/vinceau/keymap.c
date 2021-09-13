d80164a0a9b3b18daa56fa20562824593649c1b7
Vince Au <vinceau09@gmail.com>
2021-09-13 17:43:03 +1000

    feat: add meta keys

--- keyboards/moonlander/keymaps/vinceau/keymap.c
+++ keyboards/moonlander/keymaps/vinceau/keymap.c
@@ -36,12 +36,12 @@ enum custom_keycodes {
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [BASE] = LAYOUT_moonlander(
-        KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_LEFT,           KC_RGHT, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,
+        KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    WEBUSB_PAIR,       KC_RGHT, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    TG(SYMB),         TG(SYMB), KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,
         LT(NUM, KC_ESCAPE), KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_HYPR,           KC_MEH,  KC_H,    KC_J,    KC_K,    KC_L,    LT(MDIA, KC_SCLN), LGUI_T(KC_QUOT),
         KC_LSFT, KC_Z, KC_X, KC_C,    KC_V,    KC_B,                                KC_N,    KC_M,    KC_COMM, KC_DOT,  RCTL_T(KC_SLSH), KC_RSFT,
-        KC_LCTL,WEBUSB_PAIR,A(KC_LSFT),KC_LEFT, KC_RGHT,  LALT_T(KC_APP),    RCTL_T(KC_ESC),   MO(VIM),   KC_DOWN, KC_LBRC, KC_RBRC, MO(SYMB),
-                                            KC_SPC,  KC_BSPC, KC_LGUI,           KC_LALT,  KC_TAB,  KC_ENT
+        KC_LCTL, KC_LGUI, A(KC_LSFT), KC_LALT, KC_LGUI,  LALT_T(KC_APP),    RCTL_T(KC_ESC),   KC_RALT,   KC_RCTL, KC_LBRC, KC_RBRC, MO(SYMB),
+                                            KC_SPC,  KC_BSPC, KC_LGUI,           KC_RGUI,  MO(VIM),  KC_ENT
     ),
 
     [NUM] = LAYOUT_moonlander(
