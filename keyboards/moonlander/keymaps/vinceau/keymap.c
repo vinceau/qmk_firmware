cb990f63fe99926dc0be77dec6843da35d5079ff
Vince Au <vinceau09@gmail.com>
2021-09-15 14:17:27 +1000

    feat: use mac layer by default

--- keyboards/moonlander/keymaps/vinceau/keymap.c
+++ keyboards/moonlander/keymaps/vinceau/keymap.c
@@ -23,9 +23,9 @@
 
 enum layers {
     BASE,  // default layer
-    SYMB,  // symbols
+    WIN,   // windows layer
     NUM,   // number pad
-    MAC,   // mac
+    SYMB,  // symbols
     VIM,   // vim navigation
     MDIA,  // media keys
 };
@@ -37,20 +37,20 @@ enum custom_keycodes {
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [BASE] = LAYOUT_moonlander(
-        KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    WEBUSB_PAIR,       KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,
-        KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    TG(SYMB),         TG(SYMB), KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
-        LT(NUM, KC_ESCAPE), KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_HYPR,           KC_MEH,  KC_H,    KC_J,    KC_K,    KC_L,    LT(MDIA, KC_SCLN), LGUI_T(KC_QUOT),
-        KC_LSFT, KC_Z, KC_X, KC_C,    KC_V,    KC_B,                                KC_N,    KC_M,    KC_COMM, KC_DOT,  RCTL_T(KC_SLSH), KC_RSFT,
-        KC_LCTL, KC_LGUI, KC_LALT, KC_LGUI, KC_SPC,  LALT_T(KC_APP),    TG(MAC),   KC_RALT,   KC_RCTL, KC_LBRC, LT(SYMB, KC_RBRC), KC_ENTER,
-                                            KC_SPC,  KC_BSPC, KC_LGUI,           KC_RGUI,  MO(VIM),  KC_ENT
+        KC_GRV,             KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    XXXXXXX,            KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,
+        KC_TAB,             KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    XXXXXXX,         XXXXXXX,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
+        LT(NUM, KC_ESCAPE), KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    XXXXXXX,         XXXXXXX,    KC_H,    KC_J,    KC_K,    KC_L,    LT(MDIA, KC_SCLN), KC_QUOT,
+        KC_LSFT,            KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                                 KC_N,    KC_M,    KC_COMM, KC_DOT,  RCTL_T(KC_SLSH), KC_RSFT,
+        KC_LCTL,            KC_LALT, KC_LGUI, XXXXXXX, KC_SPC,           LALT_T(KC_APP),  TG(WIN),             KC_RGUI, KC_RALT, KC_LBRC, LT(SYMB, KC_RBRC), KC_ENTER,
+                                            KC_SPC,  KC_BSPC, WEBUSB_PAIR,           TG(SYMB),  MO(VIM),  KC_ENT
     ),
 
-    [MAC] = LAYOUT_moonlander(
+    [WIN] = LAYOUT_moonlander(
         _______, _______, _______, _______, _______, _______, _______,           _______, _______, _______,  _______, _______, _______,  _______,
         _______, _______, _______, _______, _______, _______, _______,           _______, _______, _______, _______, _______, _______, _______,
         _______, _______, _______, _______, _______, _______, _______,           _______, _______, _______, _______, _______, _______,  _______,
         _______, _______, _______, _______, _______, _______,                             _______, _______, _______, _______, _______, _______,
-        _______, KC_LALT, KC_LGUI, _______, _______,          _______,           _______,          KC_RGUI, KC_RALT, _______, _______, _______,
+        _______, KC_LGUI, KC_LALT, _______, _______,          _______,           _______,          KC_RALT, KC_RCTL, _______, _______, _______,
                                             _______, _______, _______, _______ ,_______, _______
     ),
 
