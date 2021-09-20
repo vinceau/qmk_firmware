1212c5f9a69a01101028e54b7254e26c5834ab4c
Vince Au <vinceau09@gmail.com>
2021-09-20 12:53:00 +1000

    refactor(moonlander): adjust moonlander layer toggle keys

--- keyboards/moonlander/keymaps/vinceau/keymap.c
+++ keyboards/moonlander/keymaps/vinceau/keymap.c
@@ -39,10 +39,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [BASE] = LAYOUT_moonlander(
         KC_GRV,             KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    XXXXXXX,            KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,
         KC_TAB,             KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    XXXXXXX,         XXXXXXX,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
-        LT(NUM, KC_ESCAPE), KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    XXXXXXX,         XXXXXXX,    KC_H,    KC_J,    KC_K,    KC_L,    LT(MDIA, KC_SCLN), KC_QUOT,
-        KC_LSFT,            KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                                 KC_N,    KC_M,    KC_COMM, KC_DOT,  RCTL_T(KC_SLSH), KC_RSFT,
-        KC_LCTL,            KC_LALT, KC_LGUI, XXXXXXX, KC_SPC,           LALT_T(KC_APP),  TG(WIN),             KC_RGUI, KC_RALT, KC_LBRC, LT(SYMB, KC_RBRC), KC_ENTER,
-                                            KC_SPC,  KC_BSPC, WEBUSB_PAIR,           TG(SYMB),  MO(VIM),  KC_ENT
+        LT(NUM, KC_ESCAPE), KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    XXXXXXX,         XXXXXXX,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
+        KC_LSFT,            KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                                 KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
+        KC_LCTL,            KC_LALT, KC_LGUI, XXXXXXX, KC_SPC,           TG(WIN),        TG(SYMB),             KC_RGUI, KC_RALT, KC_LBRC, KC_RBRC, MO(SYMB),
+                                            KC_SPC,  KC_BSPC, WEBUSB_PAIR,           MO(MDIA),  MO(VIM),  KC_ENT
     ),
 
     [WIN] = LAYOUT_moonlander(
