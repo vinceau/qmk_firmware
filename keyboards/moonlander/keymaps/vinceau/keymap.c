a71800a65878a20089a9aa7546af5e9c96dd28d7
Vince Au <vinceau09@gmail.com>
2021-09-27 19:31:11 +1000

    feat(moonlander): add media function key modifier

--- keyboards/moonlander/keymaps/vinceau/keymap.c
+++ keyboards/moonlander/keymaps/vinceau/keymap.c
@@ -37,12 +37,12 @@ enum custom_keycodes {
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [BASE] = LAYOUT_moonlander(
-        KC_GRV,             KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    XXXXXXX,            KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,
+        KC_GRV,             KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    WEBUSB_PAIR,        KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,
         KC_TAB,             KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    XXXXXXX,         XXXXXXX,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
         LT(NUM, KC_ESCAPE), KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    XXXXXXX,         XXXXXXX,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
         KC_LSFT,            KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                                 KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
         KC_LCTL,            KC_LALT, KC_LGUI, XXXXXXX, KC_SPC,           TG(WIN),        TG(SYMB),             KC_RGUI, KC_RALT, KC_LBRC, KC_RBRC, MO(SYMB),
-                                            KC_SPC,  KC_BSPC, WEBUSB_PAIR,           MO(MDIA),  MO(VIM),  KC_ENT
+                                            KC_SPC,  KC_BSPC, MO(MDIA),           MO(MDIA),  MO(VIM),  KC_ENT
     ),
 
     [WIN] = LAYOUT_moonlander(
@@ -65,7 +65,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [SYMB] = LAYOUT_moonlander(
         VRSN,    KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   _______,           KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,
-        _______, KC_EXLM, KC_AT,   KC_LCBR, KC_RCBR, KC_PIPE, _______,           _______, _______,   _______, _______, _______, _______, _______,
+        _______, KC_EXLM, KC_AT,   KC_LCBR, KC_RCBR, KC_PIPE, _______,           _______, _______, _______, _______, _______, _______, _______,
         _______, KC_HASH, KC_DLR,  KC_LPRN, KC_RPRN, KC_GRV,  _______,           RGB_TOG, _______, _______, _______, _______, _______, _______,
         _______, KC_PERC, KC_CIRC, KC_LBRC, KC_RBRC, KC_TILD,                             KC_AMPR, _______, _______, _______, _______, _______,
         EEP_RST, _______, _______, _______, _______,          RGB_VAI,           _______,          _______, _______, _______, _______,  _______,
